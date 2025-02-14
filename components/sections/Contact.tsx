de463ccaaff5cca322a533bf80e875b7672d5fa2
Juli <jjulisanz@gmail.com>
2025-02-14 13:48:03 -0300

    contact form fix

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -26,8 +26,11 @@ const Contact = () => {
   useEffect(() => {
     if (state.succeeded) {
       setIsSuccess(true);
+      console.log("Message sent successfully!");
+    } else if (state.errors) {
+      console.log("error:", state.errors);
     }
-  }, [state.succeeded]);
+  }, [state.succeeded, state.errors]);
 
   return (
     <LazyMotion features={domAnimation}>
@@ -41,11 +44,11 @@ const Contact = () => {
           animate={isInView && "visible"}
           variants={cont}
           ref={ref}
-          className="w-full max-w-[95%] max-h-[95vh] mx-auto flex flex-col md:flex-row justify-center items-center gap-10 lg:gap-4 backdrop-blur-md bg-primero/50 p-4 md:p-6 relative rounded-xl"
+          className="w-full max-w-[95%] max-h-[95vh] mx-auto flex flex-col md:flex-row justify-center items-center  backdrop-blur-md bg-primero/50 p-4 md:p-6 relative rounded-xl"
         >
           <Background isInView={isInView} />
 
-          <m.div className="w-full flex flex-col gap-6 sm:gap-10 items-center justify-center ">
+          <m.div className="w-full flex flex-col gap-6 sm:gap-8 items-center justify-center ">
             <NoiseText
               text={language === "en" ? "CONTACT US" : "CONTACTANOS"}
               addClass="tracking-wider font-bold text-center neon font-primary"
@@ -123,74 +126,75 @@ const Contact = () => {
             {/* FORM */}
             <form
               onSubmit={handleSubmit}
-              className="w-full lg:max-w-[950px] flex flex-wrap items-center justify-center gap-4"
+              className="w-full lg:max-w-[950px] flex flex-col items-center justify-center gap-6"
             >
-              {/* Name */}
-              <NoiseText
-                text={
-                  <div className="flex flex-col gap-2  ">
-                    <label
-                      htmlFor="name"
-                      className="text-cuarto font-bold font-secondary text-fluidBig"
-                      style={{ textShadow: "0px 0px 10px var(--cuarto)" }}
-                    >
-                      {language === "en" ? "Your Name" : " Tu Nombre"}
-                    </label>
-                    <input
-                      onWheel={(e) => e.stopPropagation()}
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder={
-                        language === "en"
-                          ? "What's your name?"
-                          : "¿Cuál es tu nombre?"
-                      }
-                      className="bg-primero/50 py-2 md:py-4 px-6 text-quinto placeholder-quinto rounded-lg font-medium font-secondary text-fluidBig focus:outline-none border border-cuarto focus:border-quinto focus:bg-primero focus:scale-95 transition ease-in-out duration-300"
-                      style={{ boxShadow: "0 1px 5px 1px var(--quinto)" }}
-                    />
-                  </div>
-                }
-                variantProp={opacityAnimation}
-                fontSizeClass="flex-grow"
-              />
-              <ValidationError
-                prefix="Name"
-                field="name"
-                errors={state.errors}
-              />
+              <div className="flex flex-wrap w-full gap-4">
+                {/* Name */}
+                <NoiseText
+                  text={
+                    <div className="flex flex-col gap-2  ">
+                      <label
+                        htmlFor="name"
+                        className="text-cuarto font-bold font-secondary text-fluidBig"
+                        style={{ textShadow: "0px 0px 10px var(--cuarto)" }}
+                      >
+                        {language === "en" ? "Your Name" : " Tu Nombre"}
+                      </label>
+                      <input
+                        onWheel={(e) => e.stopPropagation()}
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder={
+                          language === "en"
+                            ? "What's your name?"
+                            : "¿Cuál es tu nombre?"
+                        }
+                        className="bg-primero/50 py-2 md:py-4 px-6 text-quinto placeholder-quinto rounded-lg font-medium font-secondary text-fluidBig focus:outline-none border border-cuarto focus:border-quinto focus:bg-primero focus:scale-95 transition ease-in-out duration-300"
+                        style={{ boxShadow: "0 1px 5px 1px var(--quinto)" }}
+                      />
+                    </div>
+                  }
+                  variantProp={opacityAnimation}
+                  fontSizeClass="flex-grow"
+                />
+                <ValidationError
+                  prefix="Name"
+                  field="name"
+                  errors={state.errors}
+                />
 
-              {/* Email */}
-              <NoiseText
-                text={
-                  <div className="flex flex-col gap-2">
-                    <label
-                      htmlFor="email"
-                      className="text-cuarto font-bold font-secondary text-fluidBig"
-                      style={{ textShadow: "0px 0px 10px var(--cuarto)" }}
-                    >
-                      {language === "en" ? "Your Email" : "Tu Email"}
-                    </label>
-                    <input
-                      onWheel={(e) => e.stopPropagation()}
-                      type="email"
-                      name="email"
-                      id="email"
-                      placeholder="What's your email?"
-                      className="bg-primero/50 py-2 md:py-4 px-6 text-quinto placeholder-quinto rounded-lg font-medium font-secondary text-fluidBig focus:outline-none border border-cuarto focus:border-quinto focus:bg-primero focus:scale-95 transition ease-in-out duration-300"
-                      style={{ boxShadow: "0 1px 5px 1px var(--quinto)" }}
-                    />
-                  </div>
-                }
-                variantProp={opacityAnimation}
-                fontSizeClass="flex-grow"
-              />
-              <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-              />
-
+                {/* Email */}
+                <NoiseText
+                  text={
+                    <div className="flex flex-col gap-2">
+                      <label
+                        htmlFor="email"
+                        className="text-cuarto font-bold font-secondary text-fluidBig"
+                        style={{ textShadow: "0px 0px 10px var(--cuarto)" }}
+                      >
+                        {language === "en" ? "Your Email" : "Tu Email"}
+                      </label>
+                      <input
+                        onWheel={(e) => e.stopPropagation()}
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="What's your email?"
+                        className="bg-primero/50 py-2 md:py-4 px-6 text-quinto placeholder-quinto rounded-lg font-medium font-secondary text-fluidBig focus:outline-none border border-cuarto focus:border-quinto focus:bg-primero focus:scale-95 transition ease-in-out duration-300"
+                        style={{ boxShadow: "0 1px 5px 1px var(--quinto)" }}
+                      />
+                    </div>
+                  }
+                  variantProp={opacityAnimation}
+                  fontSizeClass="flex-grow"
+                />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
+              </div>
               {/* Message */}
               <NoiseText
                 text={
@@ -218,32 +222,33 @@ const Contact = () => {
                   </div>
                 }
                 variantProp={opacityAnimation}
-                fontSizeClass="flex-grow"
+                fontSizeClass="w-full"
               />
               <ValidationError
                 prefix="Message"
                 field="message"
                 errors={state.errors}
               />
+
+              {/* Button */}
+              <NoiseText
+                text={
+                  <button
+                    type="submit"
+                    disabled={state.submitting}
+                    className="mx-auto uppercase border border-cuarto text-quinto text-sm font-primary  cursor-pointer flex items-center justify-center relative z-0 btn w-[250px] h-[40px]"
+                    style={{
+                      filter: "drop-shadow(0 1px 3px)",
+                      textShadow: "0px 0px 5px var(--cuarto)",
+                    }}
+                  >
+                    {state.submitting ? "Sending..." : "Transmit Message"}
+                  </button>
+                }
+                variantProp={opacityAnimation}
+              />
+              <ValidationError errors={state.errors} />
             </form>
-            {/* Button */}
-            <NoiseText
-              text={
-                <button
-                  type="submit"
-                  disabled={state.submitting}
-                  className="mx-auto uppercase border border-cuarto text-quinto text-sm font-primary  cursor-pointer flex items-center justify-center relative z-0 btn w-[250px] h-[40px]"
-                  style={{
-                    filter: "drop-shadow(0 1px 3px)",
-                    textShadow: "0px 0px 5px var(--cuarto)",
-                  }}
-                >
-                  {state.submitting ? "Sending..." : "Transmit Message"}
-                </button>
-              }
-              variantProp={opacityAnimation}
-            />
-            <ValidationError errors={state.errors} />
           </m.div>
         </m.div>
         {/* SUCCES MESSAGE */}
